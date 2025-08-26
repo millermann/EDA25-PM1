a7848630b8d3e737508408b1eb6c89a5f535dcda
millermann <118037014+millermann@users.noreply.github.com>
2025-08-25 22:46:41 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include "LSO.h"
 #include "alumno.h"
 
+#define nombreArchivo "Archivo.txt"
+
 int main()
 {
     int opcion = -1, check_resp = 0, resultado = 0, resp = 0;
@@ -50,22 +52,26 @@ int main()
                 alumno x;
 
                 printf("\n - Ingrese el codigo del alumno: ");
-                scanf("%s", x.codigo);
-
-                // pasar a mayus el codigo
+                fflush(stdin);
+                scanf("%7[^\n]", x.codigo);
+                strAMayus(x.codigo);
 
                 while (pertenece(x.codigo, &listaUsada) && !isEmpty(listaUsada))
                 {
                     printf("\n\a # El codigo de alumno ingresado ya existe en la base, por favor ingrese otro...");
                     printf("\n - Ingrese el codigo del alumno: ");
-                    scanf("%s", x.codigo);
+                    fflush(stdin);
+                    scanf("%7[^\n]", x.codigo);
+                    strAMayus(x.codigo);
                 }
 
                 printf("\n - Ingrese el nombre completo del alumno: ");
-                scanf(" %[^\n]", x.nombreCompleto);
+                fflush(stdin);
+                scanf("%80[^\n]", x.nombreCompleto);
 
                 printf("\n - Ingrese el correo del alumno: ");
-                scanf("%s", x.mail);
+                fflush(stdin);
+                scanf("%23[^\n]", x.mail);
 
                 printf("\n - Ingrese la nota del alumno: ");
                 check_resp = scanf("%d", &x.nota);
@@ -146,7 +152,9 @@ int main()
                 char codigo[cod_alumno_size];
 
                 printf("\n - Ingrese el codigo del alumno a eliminar: ");
-                scanf("%s", codigo);
+                fflush(stdin);
+                scanf("%7[^\n]", codigo);
+                strAMayus(codigo);
                 resultado = baja(codigo, &listaUsada);
 
                 switch (resultado)
@@ -182,8 +190,9 @@ int main()
                 int exito;
                 char codigo[cod_alumno_size];
                 printf("\n - Ingrese el codigo del alumno: ");
-                scanf("%s", codigo);
-                // control mayus
+                fflush(stdin);
+                scanf("%7[^\n]", codigo);
+                strAMayus(codigo);
                 exito = modificar(codigo, &listaUsada);
                 switch (exito)
                 {
@@ -212,29 +221,22 @@ int main()
                 printf("\n\a # No hay alumnos cargados en la base...");
             else
             {
-                int exito;
+                alumno *x;
                 char codigo[cod_alumno_size];
                 printf("\n - Ingrese el codigo del alumno: ");
-                scanf("%s", codigo);
-                // control mayus
-                exito = modificar(codigo, &listaUsada);
-                switch (exito)
-                {
-                case -1:
+                fflush(stdin);
+                scanf("%7[^\n]", codigo);
+                strAMayus(codigo);
+
+                if ((x = evocar(codigo, &listaUsada)) == NULL)
                     printf("\n\a # No se pudo encontrar al alumno...");
-                    break;
-                case 0:
-                    printf("\n\a # No se han efectuado los cambios...");
-                    break;
-                case 1:
-                    printf("\n # Se han efectuado los cambios...");
-                }
+                else
+                    mostrarDatos(*x);
             }
             printf("\n\n - Pulse para volver al menu...");
             fflush(stdin);
             getchar();
             break;
-
         }
 
         case 5:
@@ -248,10 +250,16 @@ int main()
             else
             {
                 if (memorizar("Alumnos.txt", &listaUsada, info) == 1)
-                    printf("\n\a # Ocurrio un error al importar el archivo \"alumnos.txt\"...");
+                    printf("\n\a # Ocurrio un error al importar el archivo \"%s\"...", nombreArchivo);
                 else
                 {
-                    printf("\n # Se ha importado correctamente los datos del archivo \"alumnos.txt\"...");
+
+                    if (isFull(listaUsada))
+                        printf("\n\a # Memoria llena: No se ha podido importar la totalidad de");
+                    else
+                        printf("\n # Se ha importado correctamente");
+                    printf(" los datos del archivo \"%s\"...", nombreArchivo);
+
                     printf("\n\t + Alumnos cargados: %d", info[0]);
                     printf("\n\t + Omitidos (Codigo no valido): %d", info[1]);
                 }
@@ -273,26 +281,7 @@ int main()
             }
             else
             {
-                // esta bien usar estas funciones aca??
-                int aux = 0;
-
-                resetCur(&listaUsada);
-                while (isOos(listaUsada) != 1)
-                {
-                    if (aux > 3)
-                    {
-                        printf("\n\n - Pulse para continuar...");
-                        fflush(stdin);
-                        getchar();
-                        system("cls");
-                        printf("\n # # # #   M O S T R A R   A L U M N O S   # # # #\n");
-                        aux = 0;
-                    }
-                    aux++;
-                    mostrarDatos(peek(listaUsada));
-                    forward(&listaUsada);
-                }
-                printf("\n\n # No hay mas alumnos para mostrar...");
+                mostrarEstructura(&listaUsada);
             }
             printf("\n\n - Pulse para volver al menu...");
             fflush(stdin);
